78e53f5171285275709e7ea1a8f2927894aa702b
vetrovoy <v3trovoy@gmail.com>
2023-02-02 17:50:34 +0300

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,18 @@ module.exports = {
           filename: 'fonts/[name][ext]'
         }
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
     ],
   },
   plugins: [
