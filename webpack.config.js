1895cbedbab690df6341c2683c04cd9d4ba37dc5
vetrovoy <v3trovoy@gmail.com>
2023-02-02 17:49:06 +0300

    Add generator for assets

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: 'images/[name][ext]',
     clean: true,
   },
   devServer: {
@@ -50,10 +51,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
       },
     ],
   },
