6be6986055b4baac0c8be65996c5069a1fb72e8b
devwpnew <dev@wpnew.ru>
2023-02-02 13:26:34 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './style.css'
+
+// console.log(process.env.NODE_ENV);
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require("path");
+
+const mode = process.env.NODE_ENV;
+
+module.exports = {
+  mode: mode,
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          mode === 'production' ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      browsers: 'last 2 versions',
+                    },
+                    "autoprefixer",
+                    "postcss-simple-vars"
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Test",
+      filename: "index.html",
+      template: "src/index.html",
+      minify: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+};
